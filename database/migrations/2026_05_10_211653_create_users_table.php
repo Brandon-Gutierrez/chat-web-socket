ad3003895d7a79c8ef0fb15609e16e92e2068f65
Brandon-Gutierrez <brandonvel231@gmail.com>
2026-05-11 10:55:18 -0400

    cambio de type para la columna avatar en users, debido a que suele ser muy extenso se cambio de varchar a text

--- database/migrations/2026_05_10_211653_create_users_table.php
+++ database/migrations/2026_05_10_211653_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('google_id')->nullable(); // Para Socialite
+            $table->string('google_id')->nullable(); // String grande
             $table->string('avatar')->nullable();
             $table->timestamps();
         });
